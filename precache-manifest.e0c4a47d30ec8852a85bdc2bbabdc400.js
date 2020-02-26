dcbf18e9cbef5b95535f880139356b0b1a471409
unknown <pateriyagaurang1999@gmail.com>
2020-02-26 19:30:52 +0530

    first commit

---
+++ precache-manifest.e0c4a47d30ec8852a85bdc2bbabdc400.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f70b6a1fc2ee99d5feb43eacb052a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "629e875500d28e2f6d9c",
+    "url": "/static/css/2.74416d59.chunk.css"
+  },
+  {
+    "revision": "ffece97ef7e38b1cdbf8",
+    "url": "/static/css/main.9e499572.chunk.css"
+  },
+  {
+    "revision": "629e875500d28e2f6d9c",
+    "url": "/static/js/2.ae570338.chunk.js"
+  },
+  {
+    "revision": "ffece97ef7e38b1cdbf8",
+    "url": "/static/js/main.03e36f6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e9698d5028c7d952751fadd5416f3b0",
+    "url": "/static/media/bg-circles-1.1e9698d5.svg"
+  }
+]);
