6ab2eb904bc301c6bd344c324fdf71f713b6a7ec
unknown <pateriyagaurang1999@gmail.com>
2020-03-18 15:37:01 +0530

    this is the build  with the n26 form

---
+++ precache-manifest.288484fa02d0e5d02fef3e1161836ff7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cbdb496bdcf93d3e4a1bc93c7fca161",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2390535610af0069b030",
+    "url": "/static/css/2.9bc2b7f6.chunk.css"
+  },
+  {
+    "revision": "d514f318aef42b3077a3",
+    "url": "/static/css/main.f451aa1c.chunk.css"
+  },
+  {
+    "revision": "2390535610af0069b030",
+    "url": "/static/js/2.dbf85f8d.chunk.js"
+  },
+  {
+    "revision": "d514f318aef42b3077a3",
+    "url": "/static/js/main.4bf00176.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e9698d5028c7d952751fadd5416f3b0",
+    "url": "/static/media/bg-circles-1.1e9698d5.svg"
+  },
+  {
+    "revision": "6bfb5ac9ef5116e9cd1a052d187e5e91",
+    "url": "/static/media/bg.6bfb5ac9.svg"
+  },
+  {
+    "revision": "f418fa902f74f4411f9566b8f9c33b86",
+    "url": "/static/media/cart.f418fa90.png"
+  },
+  {
+    "revision": "acb846ef9084576aa2b6a57f38e25f97",
+    "url": "/static/media/color.acb846ef.png"
+  },
+  {
+    "revision": "6fa1fde44169b09a3819cc65c64442da",
+    "url": "/static/media/flag.6fa1fde4.png"
+  },
+  {
+    "revision": "539cdc9299496eab94e195b7bbe94374",
+    "url": "/static/media/logout.539cdc92.svg"
+  },
+  {
+    "revision": "d23f82be0b982868f08af5d517dd6dcd",
+    "url": "/static/media/search.d23f82be.png"
+  },
+  {
+    "revision": "ba0cc364c5acf6ebb34869f776ef0d6a",
+    "url": "/static/media/send.ba0cc364.png"
+  },
+  {
+    "revision": "ac51690224383b3652a489a03d3595c1",
+    "url": "/static/media/user-avatar.ac516902.svg"
+  },
+  {
+    "revision": "8be1ab909b7bd037f77039a3f6889c28",
+    "url": "/static/media/user.8be1ab90.png"
+  }
+]);
