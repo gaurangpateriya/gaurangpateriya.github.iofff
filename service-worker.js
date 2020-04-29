010c302a1673c905d036a31e4368c21da87b6f1d
unknown <pateriyagaurang1999@gmail.com>
2020-04-29 23:20:35 +0530

    sdfdsf

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.add4051e3e7ef5651f2442348ef4dfd2.js"
+  "/precache-manifest.3fac61407af2e1e041ca1a18108c2db2.js"
 );
 
 self.addEventListener('message', (event) => {
