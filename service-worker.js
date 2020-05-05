543f5c3092db82bd060b40b5275413d01a6d50a0
unknown <pateriyagaurang1999@gmail.com>
2020-05-05 11:54:21 +0530

    Dgfg

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.956f8173ea0fd6992848d49206a69b43.js"
+  "/precache-manifest.1f583380463c561bdad572eabf975efa.js"
 );
 
 self.addEventListener('message', (event) => {
