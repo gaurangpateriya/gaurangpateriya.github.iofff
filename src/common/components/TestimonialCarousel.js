0eb4174834e4ba3eb733e8d0e8c9e6fb139cf27d
unknown <pateriyagaurang1999@gmail.com>
2020-11-10 16:31:18 +0530

    changed the styling of the testimnial caroulse

--- src/common/components/TestimonialCarousel.js
+++ src/common/components/TestimonialCarousel.js
@@ -6,60 +6,59 @@ import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
 
 import './styles.css';
 
-import {  testimonialData } from '../../constants/data';
+import { testimonialData } from '../../constants/data';
 
 // const testimonial 
 const CourseDetails = () => {
 	// console.log(our_team);
-	const [ mobileView, setMobileView ] = useState(false);
+	const [mobileView, setMobileView] = useState(false);
 
-	const resize = () =>{
+	const resize = () => {
 		setMobileView(window.innerWidth <= 576);
 	};
-	useEffect(()=>{
+	useEffect(() => {
 		resize();
 		window.addEventListener('resize', () => resize());
 		return () => {
 			window.removeEventListener('resize', () => resize());
 		};
-	},[]);
+	}, []);
 
-	
+
 	const testimonialSettings = {
 		dots: true,
 		infinite: true,
 		speed: 500,
 		slidesToScroll: 1,
 		autoplay: false,
-		autoplaySpeed:4000,
+		autoplaySpeed: 4000,
 		slidesToShow: 1,
 		variableWidth: !mobileView,
 	};
-	
+
 	return (
-		<>							
+		<>
 			<div className='testimonial-carousel'>
 				<div className='heading-div'>
-					<p className='txt'>WHAT OUR STUDENTS HAVE TO SAY
-</p>
+					<p className='txt'>WHAT OUR STUDENTS HAVE TO SAY</p>
 					<p className='watermark'>Testimonials</p>
 				</div>
 				{
 					mobileView ? (
 						<Slider {...testimonialSettings} >
 							{
-								testimonialData.map((t,i) => (
-									<a href={t.linkedInURL} className='mentor' style={{width: Math.min(window.innerWidth- 10, 400)}} >
+								testimonialData.map((t, i) => (
+									<a href={t.linkedInURL} key={i} className='mentor' style={{ width: Math.min(window.innerWidth - 10, 400) }} >
 										<div className='quote-svg'>
-											<FormatQuoteIcon/>
+											<FormatQuoteIcon />
 										</div>
 										<div className='discreption'>
 											<p> "{t.testimonial}" </p>
 										</div>
 										<div className='header'>
-											<img src={t.photo} alt="" />	
+											<img src={t.photo} alt="" />
 										</div>
-									
+
 										<div className='heading'>
 											<div className='name'>
 												<a href={t.linkedInURL}>
@@ -73,45 +72,45 @@ const CourseDetails = () => {
 											</div>
 										</div>
 									</a>
-								))    
+								))
 							}
 						</Slider>
 					) : (
-						<div className='flex justify-between'>
-							{
-								testimonialData.map((t,i) => (
-									<a href={t.linkedInURL} className='mentor' style={{width: Math.min(window.innerWidth- 10, 400)}} >
-										<div className='quote-svg'>
-											<FormatQuoteIcon/>
-										</div>
-										<div className='discreption'>
-											<p> "{t.testimonial}" </p>
-										</div>
-										<div className='header'>
-											<img src={t.photo} alt="" />	
-										</div>
-									
-										<div className='heading'>
-											<div className='name'>
-												<a href={t.linkedInURL}>
-													<LinkedInIcon />
-												</a>
-												<p>{t.name}</p>
+							<div className='flex justify-between'>
+								{
+									testimonialData.map((t, i) => (
+										<a href={t.linkedInURL} key={i} className='mentor' style={{ width: Math.min(window.innerWidth - 10, 400) }} >
+											<div className='quote-svg'>
+												<FormatQuoteIcon />
 											</div>
-											<div className='company'>
-												<p>Placed in {t.company}</p>
-												<p className=''>{t.package}</p>
+											<div className='discreption'>
+												<p> "{t.testimonial}" </p>
 											</div>
-										</div>
-									</a>
-								))    
-							}
-						</div>
-					)
+											<div className='header'>
+												<img src={t.photo} alt="" />
+											</div>
+
+											<div className='heading'>
+												<div className='name'>
+													<a href={t.linkedInURL}>
+														<LinkedInIcon />
+													</a>
+													<p>{t.name}</p>
+												</div>
+												<div className='company'>
+													<p>Placed in {t.company}</p>
+													<p className=''>{t.package}</p>
+												</div>
+											</div>
+										</a>
+									))
+								}
+							</div>
+						)
 				}
-				
+
 			</div>
-			
+
 		</>
 	);
 };
