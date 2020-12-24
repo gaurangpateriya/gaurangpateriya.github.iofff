16d33596041e39e23184a86e4d940310b19d2a46
Gaurang Pateriya <pateriyagaurang1999@gmail.com>
2020-12-24 20:32:11 +0530

    added the ISA PAge

--- src/components/Isa/Landing/Landing.js
+++ src/components/Isa/Landing/Landing.js
@@ -1,12 +1,18 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+// import { Link } from 'react-router-dom';
 import manIcon from '../../../assets/Images/ISA/icon1.png';
+import cover from '../../../assets/Images/ISA/cover.jpg';
+import jobSecurity from '../../../assets/Images/ISA/jobSecurity.jpg';
+import payment from '../../../assets/Images/ISA/payment.png';
+import nomoney from '../../../assets/Images/ISA/nomoney.jpg';
+import job from '../../../assets/Images/ISA/job.jpg';
+
 import './Landing.css';
 import Slider from './Slider';
 
 const Landing = () => {
   return (
-    <Fragment>
+    <>
       <div className='head'>
         <h1 className='white'>YAV Technology's</h1>
         <p className='fs-50 white'>Income sharing agreement (ISA)</p>
@@ -14,9 +20,9 @@ const Landing = () => {
 
       {/* ------ */}
 
-      <div className='row bg-light'>
+      <div className='row '>
         <div className='col-2'>
-          <img src={manIcon} alt='image' />
+          <img src={cover} alt='image' />
         </div>
         <div className='col-2'>
           <h1>What is ISA?</h1>
@@ -28,13 +34,13 @@ const Landing = () => {
 
       {/* ------ */}
 
-      <div className='row p-3'>
+      <div className='row p-3 bg-light'>
         <div className='align'>
           <h1>4 THINGS TO KNOW ABOUT THE YAV's ISA</h1>
         </div>
         <div className='col-4 '>
           <div className='bg-light p-1'>
-            <img src={manIcon} alt='image' />
+            <img src={job} alt='image' />
             <h4>Rs. 0 till you get a job</h4>
             <p className='p-1'>
               With YAV Technology, you do not pay a single penny till you get a job! No registration fees, deposits or monthly fees or any other hidden charges.
@@ -43,7 +49,7 @@ const Landing = () => {
         </div>
         <div className='col-4 '>
           <div className='bg-light p-1'>
-            <img src={manIcon} alt='image' />
+            <img src={payment} alt='image' />
             <h4>Pay amount based on what you get</h4>
             <p className='p-1'>
               The amount we charge is a percentage of what you earn. Why? This keeps it fair for students with all kinds of job offers without burdening anyone. Yes, we care.
@@ -52,7 +58,7 @@ const Landing = () => {
         </div>
         <div className='col-4 '>
           <div className='bg-light p-1'>
-            <img src={manIcon} alt='image' />
+            <img src={jobSecurity} alt='image' />
             <h4>Job Loss Security</h4>
             <p className='p-1'>
               Job markets are uncertain, we know. In case you lose your job, your ISA payments stop. You can then onboard our program again and we will only be charging you again after you get another job. Don't worry, your prior payments, if any, will be adjusted.
@@ -61,7 +67,7 @@ const Landing = () => {
         </div>
         <div className='col-4 '>
           <div className='bg-light p-1'>
-            <img src={manIcon} alt='image' />
+            <img src={nomoney} alt='image' />
             <h4>Stop Paying if you reach the cap</h4>
             <p className='p-1'>
               Got an awesome package? Well we don't have a minimum ISA payout but do have a maximum! If you hit 1 Lakh, you don't need to pay anything further.
@@ -73,7 +79,7 @@ const Landing = () => {
       {/* ------ */}
 
       <div className='p-3'>
-        <div className='row'>
+        <div className='row pb5'>
           <div className='align'>
             <h1>Let's look at the numbers</h1>
           </div>
@@ -113,13 +119,13 @@ const Landing = () => {
 
         {/*--------*/}
 
-        <div className='bg-light align p-3'>
+        {/* <div className='bg-light align p-3'>
           <p className='p-2 fs-30'>Start learning with YAV today!</p>
           <Link to='/talk-to-mentor' className='btn p-1'>Apply now for free</Link>
 
-        </div>
+        </div> */}
       </div>
-    </Fragment>
+    </>
   );
 };
 export default Landing;
